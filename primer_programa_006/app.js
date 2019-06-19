1f083f2a9e74dd04a3c0a6edd25463d68a851c70
mclein <magindev@gmail.com>
2019-06-18 23:09:58 -0400

    Aprendiendo JS 2019 QK001 primeros pasos

---
+++ primer_programa_006/app.js
@@ -0,0 +1,11 @@
+// primer programa
+
+//let nombre = prompt('Cual es tu nombre');
+//let edad = prompt('Cual es tu edad');
+
+//let d = new Date();
+//document.getElementById('app').innerHTML = 'Buenas Nuevas '+nombre+' tu edad es:'+edad;
+//document.getElementById('hora').innerHTML = d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
+//
+var nombre = 'jose';
+console.log(nombre);
---
+++ variable_009/app.js
@@ -0,0 +1,30 @@
+// hay 3 formas de ceclarar variables var - let -const
+// var la forma antigua
+// let-const = forma nueva
+var nombre = 'jose';//puede ser comillas dobles o simples "" o ''
+nombre = 'maria';//sobreescribe el primer nombre
+var nombre = "Juan";//sobre escribe el segundo nombre
+console.log(nombre);
+
+
+/*
+undifined ejemplo
+ 
+var carrito;
+console.log(carrito);
+ */
+
+//inicializar variable
+  	var carrito;
+//	inicializar varias variables
+
+    var carrito = 'libro',
+        carrito1= 'libro2',
+        carrito2= 'libro3';
+
+        console.log(carrito2);
+//nomesclatura variables
+
+	var producto;
+	//var 2019producto; NO PUEDE TENER NUMEROS O CARACTERES AL PRINCIPIO        
+	
