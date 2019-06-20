69af76813be1ef013a7b93c2344d04602682ba56
Ecobit <magindev@gmail.com>
2019-06-19 23:31:59 -0400

    Archivos Nuevos QK002

---
+++ variable_010_let_const/app.js
@@ -0,0 +1,11 @@
+// hay 3 formas de ceclarar variables var - let -const
+// var la forma antigua
+// let-const = forma nueva
+// let permite declarar variable vacia y inicializar una variable o reasignar valor 
+// const = Define una variable constante no permite inicializar vacia no reasignar valor
+
+let producto = 'Televisor',
+    prod2 = 'Lavadora',
+    prod3 = 'Cama';
+//let producto = 'Lavadora'; no se puede volover a declarar
+console.log(prod3);
